292effd704e43a8acc3d1e334b56985d32593b6e
MSAnewl <43391094+MSAnewl@users.noreply.github.com>
2018-09-24 22:02:00 +0300

    calculator

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,20 @@
 package com.company;
 
-import java.awt.print.Printable;
+import java.io.PrintStream;
+import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.print("Hello World!");
+    public static void main(String[] args) {           //В методе вводится и вычисляется сумма двух дробных чисел
+        System.out.println("Введите первое число");
+
+        Scanner scanner = new Scanner(System.in);
+        float a = scanner.nextFloat();
+        System.out.println("Введите второе число");
+        float b = scanner.nextFloat();
+
+        float x = a + b;
+        System.out.println();
+        System.out.printf("%s + %s = %.4f", a, b, x);
     }
 }
